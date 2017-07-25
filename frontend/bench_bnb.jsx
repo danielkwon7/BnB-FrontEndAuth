36431da10f787193d243fef1b0f9902b11096722
Daniel Kwon <dhkwon@alumni.cmu.edu>
2017-07-25 14:11:37 -0700

    Added app and root components

--- frontend/bench_bnb.jsx
+++ frontend/bench_bnb.jsx
@@ -1,16 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { login, logout, signup } from './util/session_api_util';
 import configureStore from './store/store'
+import Root from './components/root'
 
 
 document.addEventListener('DOMContentLoaded', () => {
   const root = document.getElementById('root');
   const store = configureStore();
-  window.login = login ;
-  window.logout = logout;
-  window.signup = signup;
   window.getState = store.getState;
   window.dispatch = store.dispatch;
-  ReactDOM.render(<h1>Welcome to BenchBnB</h1>, root);
+  ReactDOM.render(<Root store={ store }/>, root);
 });
