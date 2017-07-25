3952dde69d4b1ed4adc95e22a0b53ecbe7d96b2f
Daniel Kwon <dhkwon@alumni.cmu.edu>
2017-07-25 11:39:59 -0700

    Set up users model

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,8 +3,10 @@ class User < ApplicationRecord
 
   validates :username, :password_digest, :session_token, presence: true
   validates :username, uniqueness: true
+  validates :password, length: { minimum: 6 }, allow_nil: :true
 
-
+  after_initialize :ensure_session_token
+  before_validation :ensure_session_token_uniqueness
 
   def password=(password)
     self.password_digest = BCrypt::Password.create(password)
